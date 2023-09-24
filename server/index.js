e8c6e2bf0793490e787d8c29b80dd441257f651b
abhishekkumar <abhishekkumar0841@gmail.com>
2023-09-24 15:35:14 +0530

    client completed

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5500',
+    credentials: true,
+}))
 app.use(express.json())
 app.use(cookieParser())
 
